1983968ee46d4060140f33bba3777ac717494a1f
Morten Nilsen <morten@runsafe.no>
2018-02-22 18:31:18 +0100

    Make output size configurable

--- www/qr.php
+++ www/qr.php
@@ -2,7 +2,9 @@
 	$params = explode('/', $_SERVER['PATH_INFO']);
 	$domain = $params[1];
 	$reference = $params[2];
-	$format = explode('.', $params[3])[1];
+	$output = explode('.', $params[3]);
+	$size = is_numeric($output[0]) ? (int)$output[0] : 256;
+	$format = $output[1];
 
 	require_once('BaconQrCode/autoload.php');
 	if($format == 'png')
@@ -25,8 +27,8 @@
 	}
 
 	$url = sprintf('https://%s/#!/%s', $domain, urlencode($key));
-	$renderer->setHeight(256);
-	$renderer->setWidth(256);
+	$renderer->setHeight($size);
+	$renderer->setWidth($size);
 	$writer = new \BaconQrCode\Writer($renderer);
 	header('Content-Type: image/'.$format);
 	echo $writer->writeString($url);
