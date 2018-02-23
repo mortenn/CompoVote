e6d5e52786086249954a5eae6b9406f47cbeae6f
Morten Nilsen <mortenn@hapro.no>
2018-02-23 13:40:48 +0100

    Add API provider

---
+++ app/api.js
@@ -0,0 +1,17 @@
+angular.module('vote')
+	.factory('API',[
+		'$resource',
+		function($resource)
+		{
+			return $resource(
+				'api.php/:action',
+				{},
+				{
+					'GetContests': { method: 'GET', params: { action: 'contests' }, isArray: true },
+					'GetEntries': { method: 'GET', url: 'api.php/entries/:contest', isArray: true },
+					'GetVote': { method: 'GET', url: 'api.php/vote/:contest' },
+					'SetVote': { method: 'PUT', url: 'api.php/vote/:contest/:entry' }
+				}
+			);
+		}
+	]);
