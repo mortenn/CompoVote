c4cfb56b5eb3b9a877c83330519755f02b0afe22
Morten Nilsen <mortenn@hapro.no>
2018-02-24 00:09:08 +0100

    Put request did not send parameters properly

--- app/api.js
+++ app/api.js
@@ -10,7 +10,7 @@ angular.module('vote')
 					'GetContests': { method: 'GET', params: { action: 'contests' }, isArray: true },
 					'GetEntries': { method: 'GET', url: 'api.php/entries/:contest', isArray: true },
 					'GetVote': { method: 'GET', url: 'api.php/vote/:contest' },
-					'SetVote': { method: 'PUT', url: 'api.php/vote/:contest/:entry' }
+					'SetVote': { method: 'PUT', url: 'api.php/vote/:contest/:entry', params: {'contest':null, entry:null} }
 				}
 			);
 		}
