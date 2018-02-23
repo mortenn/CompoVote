4da6877b9ea964f90365254d1ddb1324411d920a
Morten Nilsen <mortenn@hapro.no>
2018-02-23 13:40:09 +0100

    Start work on real frontend

--- app/vote.js
+++ app/vote.js
@@ -1,13 +1,24 @@
 angular.module('vote').component(
 	'vote',
 	{
-		controller: ['$stateParams',Vote],
+		controller: ['$stateParams','API',Vote],
 		templateUrl: 'view/votes.html'
 	}
 );
 
-function Vote($stateParams)
+function Vote($stateParams, API)
 {
 	console.log($stateParams.key);
-	this.compos = [];
+	this.contests = API.GetContests();
+	this.entries = {};
+	this.votes = {};
+	this.ExpandContest = function(contest)
+	{
+		this.entries[contest.name] = API.GetEntries({contest:contest.name});
+		this.votes[contest.name] = API.GetVote({contest:contest.name});
+	};
+	this.Vote = function(contest, entry)
+	{
+		this.votes[contest.name] = API.SetVote({contest:contest.name,entry:entry.name});
+	};
 }
